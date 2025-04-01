14d35be34195795131498ab410cab1bbbb9f11f9
RaphiMuehlbacher <raphi.muehlbacher@gmail.com>
2025-04-01 21:52:11 +0200

    feat: cpu-stats

--- data.py
+++ data.py
@@ -1,5 +1,9 @@
 import psutil
 
+
 def cpu_count():
     return psutil.cpu_count()
 
+
+def cpu_stats():
+    return psutil.cpu_stats()
--- main.py
+++ main.py
@@ -11,3 +11,8 @@ async def root():
     return {"cpu_count": cpu_count}
 
 
+@app.get("/cpu-stats")
+async def cpu_stats():
+    cpu_stats = data.cpu_stats()
+    print(cpu_stats)
+    return {"cpu_stats": cpu_stats}
